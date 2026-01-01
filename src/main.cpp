190dab8cbc258c8f0c2c2a8425b0c1d2530253a4
jgkiran2003 <jgkiran2003@gmail.com>
2026-01-01 22:40:04 +0800

    Generate box and random balls

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,36 @@
 #include "physics/PhysicsObject.h"
 #include "physics/PhysicsEngine.h"
 #include "physics/BoundingSphere.h"
+#include "physics/Plane.h"
 #include "render/Renderer.h"
 
 int main(int argc, char* argv[]) {
   // Initiliase renderer and physics engine
-  Renderer renderer(800, 600);
+  Renderer renderer(820, 620);
   PhysicsEngine engine;
 
-  // Add balls
-  engine.AddObject(new PhysicsObject(new BoundingSphere(Vector2(100, 300), 20), Vector2(2, 0)));
-  engine.AddObject(new PhysicsObject(new BoundingSphere(Vector2(700, 300), 20), Vector2(-2, 0)));
+  // 4 planes facing inward to form a box
+  engine.AddObject(new PhysicsObject(new Plane(Vector2(1, 0), 10), Vector2(0, 0)));  // Left Wall
+  engine.AddObject(new PhysicsObject(new Plane(Vector2(-1, 0), -810), Vector2(0, 0))); // Right Wall
+  engine.AddObject(new PhysicsObject(new Plane(Vector2(0, 1), 10), Vector2(0, 0)));  // Ceiling
+  engine.AddObject(new PhysicsObject(new Plane(Vector2(0, -1), -610), Vector2(0, 0))); // Floor
+
+  // 50 particles randomly generated within box
+  for(int i = 0; i < 50; i++) {
+    // Random position between (50, 50) and (750, 550)
+    float startX = 50 + (rand() % 700);
+    float startY = 50 + (rand() % 500);
+
+    // Random velocity
+    float velX = (rand() % 10) - 5;
+    float velY = (rand() % 10) - 5;
+
+    BoundingSphere* particleShape = new BoundingSphere(Vector2(startX, startY), 4.0f);
+    engine.AddObject(new PhysicsObject(particleShape, Vector2(velX, velY)));
+  }
 
   // Main loop
-  float timeStep = 1.0f;
+  float timeStep = 0.16f;
 
   while (renderer.IsOpen()) {
     renderer.HandleEvents();
