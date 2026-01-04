27561fce0a5b9da3f3e863b09f8f5fc7b42e06d2
jgkiran2003 <jgkiran2003@gmail.com>
2026-01-04 15:43:59 +0800

    Revert "Optimise normaise"
    This reverts commit 5e45ec3724d6b6a44d91362ca3881ede3fe07ca3.

--- src/physics/Vector2.h
+++ src/physics/Vector2.h
@@ -22,7 +22,7 @@ class Vector2 {
 
     // Returns normalised vector
     Vector2 Normalized() const {
-        float len = LengthSquared();
+        float len = Length();
         if (len == 0) return Vector2(0, 0); // Prevent division by zero
         return Vector2(x / len, y / len);
     }
